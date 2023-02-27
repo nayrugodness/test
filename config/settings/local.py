2166cf151b8f1a082567e67321e009f3d8865dfd
nayru <nayru@gigamonos.com>
2023-02-27 14:43:54 +0000

    traefik

--- config/settings/local.py
+++ config/settings/local.py
@@ -13,7 +13,7 @@ SECRET_KEY = env(
     default="IxQhgTQIalHwa03m58IdEEC3QqK7WwlgVhUxfJptNQAQUVvnwdCjlwXLaOoTcY31",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "ec2-54-227-201-228.compute-1.amazonaws.com"]
 
 # CACHES
 # ------------------------------------------------------------------------------
