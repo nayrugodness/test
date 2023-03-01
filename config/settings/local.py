eb7d196cc044e15f5ad6be241bb782ee16807609
nayru <nayru@gigamonos.com>
2023-03-01 20:56:19 +0000

    adding ssl certificate

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,7 +14,7 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "ec2-44-212-153-105.compute-1.amazonaws.com", "gaya.live"]
-
+#ALLOWED_HOST = ["gaya.live"]
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
