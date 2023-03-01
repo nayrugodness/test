0f22a9b30e8833de4cc493fe49d9a3cf3c9248b3
nayru <nayru@gigamonos.com>
2023-03-01 18:19:04 +0000

    adding media root

--- config/settings/local.py
+++ config/settings/local.py
@@ -66,3 +66,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+ROOT_DIR = Path(_file_).resolve(strict=True).parent.parent.parent
+# djsniper/
+APPS_DIR = ROOT_DIR / "djsniper"
+MEDIA_ROOT = os.path.join(APPS_DIR, 'media')
