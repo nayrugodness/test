cd58e773a110aff41deb9203e95f63aef871e534
nayru <nayru@gigamonos.com>
2023-03-09 16:41:13 +0000

    adding true

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,7 +13,7 @@ from .base import env
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = "h6=qc&mijc&ix9pwq4eybjv-aho4-fm1pvk+_sl7ua-1+_ut"
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["ec2-44-212-153-105.compute-1.amazonaws.com", "gaya.live"])
 
